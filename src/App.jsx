1ce7d5be7178e66fd003853bfed8dbbd285fe91e
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-22 17:31:18 +0500

    athentication done with email method

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Auth } from "./components/auth";
+
+function App() {
+  return (
+    <div className="app">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCK6GJuKPrvYGAE6o_4WikkHEZ-lo46YrI",
+  authDomain: "fir-course-e19e0.firebaseapp.com",
+  projectId: "fir-course-e19e0",
+  storageBucket: "fir-course-e19e0.appspot.com",
+  messagingSenderId: "137504787809",
+  appId: "1:137504787809:web:7080cb69ddb3025d2c8920",
+  measurementId: "G-H25S2WQY6S",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
